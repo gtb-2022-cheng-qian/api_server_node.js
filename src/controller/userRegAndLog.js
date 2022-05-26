68fb1b75e047039444c554b5fc58a9c7666f5a60
Alancq98 <chengqian11222@163.com>
2022-05-26 14:17:12 +0800

    feat: centered the userRegAndLog.js errors handling to error middleware at app.js

--- src/controller/userRegAndLog.js
+++ src/controller/userRegAndLog.js
@@ -1,15 +1,13 @@
 import service from '../service/userRegAndLog.js'
 
 const register = (req, res) => {
-    service.clientRegister(req)
+    return service.clientRegister(req)
         .then(() => res.status(201).send({message: 'register success'}))
-        .catch(err => res.status(500).send({message: err}))
 }
 
 const login = (req, res) => {
-    service.clientLogin(req)
+    return service.clientLogin(req)
         .then((tokenStr) => res.status(201).send({message: 'login success', token: 'Bearer ' + tokenStr,}))
-        .catch(err => res.status(500).send({message: err}))
 }
 
 export default {
--- src/routers/userRegAndLog.js
+++ src/routers/userRegAndLog.js
@@ -7,6 +7,7 @@ import userController from "../controller/userRegAndLog.js"
 import expressJoi from "@escook/express-joi"
 // 导入验证规则对象
 import {reg_login_schema} from "../schema/user.js"
+import {routeHandler} from "../utils/routeHandler.js"
 
 // 创建路由对象
 const router = express.Router()
@@ -16,10 +17,10 @@ const router = express.Router()
 // 1.1.在注册新用户的路由中，声明局部中间件，对当前请求中携带的数据进行验证
 // 1.2.数据验证通过后，会把这次请求流转给后面的路由处理函数
 // 1.3.数据验证失败后，终止后续代码的执行，并抛出一个全局的 Error 错误，进入全局错误级别中间件中进行处理
-router.post('/register', expressJoi(reg_login_schema), userController.register)
+router.post('/register', expressJoi(reg_login_schema), routeHandler(userController.register))
 // 登录
 // 1.检测表单数据是否合法
-router.post('/login', expressJoi(reg_login_schema), userController.login)
+router.post('/login', expressJoi(reg_login_schema), routeHandler(userController.login))
 
 // 将路由对象导出
 export default router
