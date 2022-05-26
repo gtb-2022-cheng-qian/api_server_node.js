5e0fd953d592e90310250efdebb6bb2b8418af4d
Alancq98 <chengqian11222@163.com>
2022-05-26 14:08:07 +0800

    feat: centered the userInfo.js errors handling to error middleware at app.js

--- src/controller/userInfo.js
+++ src/controller/userInfo.js
@@ -1,30 +1,26 @@
 import service from '../service/userInfo.js'
 
 const getUserInfo = (req, res) => {
-    service.getBasicUserInfo(req)
+    return service.getBasicUserInfo(req)
         .then(result => res.status(200).send({message: 'get user information success', data: result}))
-        .catch(err => res.status(500).send({message: err}))
 }
 
 // 更新用户基本信息的处理函数
 const updateUserInfo = (req, res) => {
-    service.updateBasicUserInfo(req)
+    return service.updateBasicUserInfo(req)
         .then(() => res.status(201).send({message: 'update user information success'}))
-        .catch(err => res.status(500).send({message: err}))
 }
 
 // 重置密码的处理函数
 const updatePwd = (req, res) => {
-    service.resetPassword(req)
+    return service.resetPassword(req)
         .then(() => res.status(201).send({message: 'reset password success'}))
-        .catch(err => res.status(500).send({message: err}))
 }
 
 // 更新用户头像的处理函数
 const updateAvatar = (req, res) => {
-    service.resetAvatar(req)
+    return service.resetAvatar(req)
         .then(() => res.status(201).send({message: 'reset avatar success'}))
-        .catch(err => res.status(500).send({message: err}))
 }
 
 export default {
--- src/routers/userInfo.js
+++ src/routers/userInfo.js
@@ -5,16 +5,17 @@ import expressJoi from "@escook/express-joi"
 import {update_schema, updatePwd_schema, updateAvatar_schema} from "../schema/user.js"
 //导入用户信息处理函数模块
 import userInfoController from "../controller/userInfo.js"
+import {routeHandler} from "../utils/routeHandler";
 
 const router = express.Router()
 
 // 获取用户的基本信息
-router.get('/userinfo', userInfoController.getUserInfo)
+router.get('/userinfo', routeHandler(userInfoController.getUserInfo))
 // 更新用户的基本信息
-router.post('/userinfo', expressJoi(update_schema), userInfoController.updateUserInfo)
+router.post('/userinfo', expressJoi(update_schema), routeHandler(userInfoController.updateUserInfo))
 // 重置密码
-router.post('/update/pwd', expressJoi(updatePwd_schema), userInfoController.updatePwd)
+router.post('/update/pwd', expressJoi(updatePwd_schema), routeHandler(userInfoController.updatePwd))
 // 更新用户头像
-router.post('/update/avatar', expressJoi(updateAvatar_schema), userInfoController.updateAvatar)
+router.post('/update/avatar', expressJoi(updateAvatar_schema), routeHandler(userInfoController.updateAvatar))
 
 export default router
